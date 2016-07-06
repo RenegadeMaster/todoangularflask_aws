33dbfdcce7fb0f6241eed21c895a1fdbb96344d5
dirk <dirk.coetsee@gmail.com>
2016-07-06 08:24:12 +0200

    lets go

---
+++ models.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.Text)
+    done = db.Column(db.Boolean)
+    date = db.Column(db.DateTime)
+
+    def __init__(self, task, done):
+        self.task = task
+        self.done = done
+        self.date = datetime.utcnow()
+
+
+class Meeting(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+class MeetingStatus(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
